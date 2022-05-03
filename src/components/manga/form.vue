8f9641f93cc983607f6319b7f9d163990b92162d
nothing628 <yogyphang@gmail.com>
2022-05-03 11:32:52 +0700

    add upload form

---
+++ src/components/manga/form.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref("")
+const description = ref("")
+const emit = defineEmits<{ (event: "submit", title: string, description: string): void }>()
+
+const handleFormSubmit = () => {
+    emit('submit', title.value, description.value)
+}
+</script>
+
+<template>
+    <form v-on:submit.prevent="handleFormSubmit">
+        <div class="flex flex-col mb-2">
+            <label>Title :</label>
+            <input required class="border h-12 rounded-xl px-4 py-2" type="text" name="email" v-model="title" />
+        </div>
+        <div class="flex flex-col mb-2">
+            <label>Description :</label>
+            <textarea rows="3" required class="border rounded-xl px-4 py-2" name="password"
+                v-model="description" />
+        </div>
+
+        <button class="bg-cyan-500 text-white text-lg px-8 py-2 rounded-lg" type="submit">
+            Upload
+        </button>
+    </form>
+</template>
--- src/pages/upload.vue
+++ src/pages/upload.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UploadForm from '../components/manga/form.vue'
+</script>
 
 <template>
     <div class="px-8 py-2">
         <a href="/">Back</a>
         <h1 class="text-2xl mb-2">Upload</h1>
+
+        <UploadForm />
     </div>
 </template>
